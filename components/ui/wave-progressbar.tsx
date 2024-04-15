ed1ecf1f3cf7431a74ed9c5b84ff43197151b1a2
Yoannis Sánchez Soto <yossthedev@gmail.com>
2024-04-15 11:23:01 -0400

    style: :lipstick: Update Wave ProgressBar style

--- components/ui/wave-progressbar.tsx
+++ components/ui/wave-progressbar.tsx
@@ -12,7 +12,7 @@ const WaveProgressbar: React.FC<Props> = ({ value }) => {
 
   return (
     <div className="flex items-center justify-center gap-4">
-      <div className="relative flex h-72 w-32 cursor-pointer items-center justify-center overflow-hidden rounded-lg bg-[#1b1b1d] hover:bg-[#1b1b1d]/80">
+      <div className="relative flex h-72 w-32 cursor-pointer items-center justify-center overflow-hidden rounded-lg  bg-neutral-200 dark:bg-neutral-900">
         <motion.div
           animate={{ rotateZ: 360 }}
           style={{ backgroundColor: "#FF00FF", bottom: -val + "px" }}
