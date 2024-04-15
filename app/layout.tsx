00c72d3888a530924cd159206847a1dd679d32fa
Yoannis Sánchez Soto <yossthedev@gmail.com>
2024-04-15 11:51:26 -0400

    feat: :lipstick: Meteor Effect on Home Page

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Metadata } from "next"
 import { siteConfig } from "@/config/site"
 import { fontSans } from "@/lib/fonts"
 import { cn } from "@/lib/utils"
+import { Meteors } from "@/components/ui/meteors"
 import { SiteHeader } from "@/components/site-header"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
@@ -43,6 +44,8 @@ export default function RootLayout({ children }: RootLayoutProps) {
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             <div className="relative flex h-screen min-h-screen flex-col">
               <SiteHeader />
+              <Meteors number={20} />
+
               <div className="flex-1">{children}</div>
             </div>
             <TailwindIndicator />
