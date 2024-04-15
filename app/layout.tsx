2d9e7c1246954a288714aff020ffdc3bdf4cdadd
Yoannis Sánchez Soto <yossthedev@gmail.com>
2024-04-15 12:29:23 -0400

    feat: :rocket: Add Footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { siteConfig } from "@/config/site"
 import { fontSans } from "@/lib/fonts"
 import { cn } from "@/lib/utils"
 import { Meteors } from "@/components/ui/meteors"
+import { Footer } from "@/components/footer"
 import { SiteHeader } from "@/components/site-header"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
@@ -46,6 +47,8 @@ export default function RootLayout({ children }: RootLayoutProps) {
               <SiteHeader />
 
               <div className="flex-1">{children}</div>
+
+              <Footer></Footer>
             </div>
             <TailwindIndicator />
           </ThemeProvider>
