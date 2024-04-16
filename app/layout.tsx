b8cd9be4b87944a6f9f65019210c4180b2e01a80
Yoannis Sánchez Soto <yossthedev@gmail.com>
2024-04-16 04:25:27 -0400

    fix: :bug: Minor UI/UX Fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,15 +20,6 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: light)", color: "white" },
     { media: "(prefers-color-scheme: dark)", color: "black" },
   ],
-  openGraph: {
-    images: [
-      {
-        url: "/og.png",
-        width: 1920,
-        height: 1280,
-      },
-    ],
-  },
   icons: {
     icon: "/favicon.svg",
     shortcut: "/favicon-16x16.png",
--- components/ui/meteors.tsx
+++ components/ui/meteors.tsx
@@ -1,5 +1,6 @@
+"use client"
+
 import React from "react"
-import clsx from "clsx"
 
 import { cn } from "@/lib/utils"
 
@@ -17,7 +18,7 @@ export const Meteors = ({
         <span
           key={"meteor" + idx}
           className={cn(
-            "animate-meteor-effect absolute left-1/2 top-1/2 size-0.5 rotate-[215deg] rounded-full bg-slate-500 shadow-[0_0_0_1px_#ffffff10]",
+            "absolute left-1/2 top-1/2 size-0.5 rotate-[215deg] animate-meteor-effect rounded-full bg-slate-500 shadow-[0_0_0_1px_#ffffff10]",
             "before:absolute before:top-1/2 before:h-px before:w-[50px] before:-translate-y-[50%] before:bg-gradient-to-r before:from-[#64748b] before:to-transparent before:content-['']",
             className
           )}
